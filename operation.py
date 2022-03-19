8561b4b6525684d6f968fb0b6c9a3d2d4ec7e05e
Sourabh <sourabhch22@gmail.com>
2022-03-19 17:10:07 +0530

    adding operation file

---
+++ operation.py
@@ -0,0 +1,15 @@
+def add(a = 0, b = 0) :
+    print(a + b) 
+
+def sub(a = 0, b = 0) :
+    print(a - b) 
+
+def mul(a = 0, b = 0) :
+    print(a * b) 
+
+def div(a = 0, b = 1) :
+    if b == 0 :
+        print('division by zero not valid') 
+    else :
+        print(a / b) 
+
