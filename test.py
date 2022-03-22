73537a7d73ed141808b95ad562fceffc8ed1d4d5
Sourabh <sourabhch22@gmail.com>
2022-03-22 09:35:27 +0530

    changing the test file

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print('hello world')
+from datetime import date, datetime
+
+def main() :
+
+    today = date.today() 
+
+    today_date = today.strftime("%B %d, %Y") 
+
+    now = datetime.now() 
+
+    time_now = now.strftime("%H:%M:%S") 
+
+    f = open("py_test.txt", "w+", encoding = 'UTF-8')
+
+    f.write("Today's date is {} and time now is {} {}".format(today_date, time_now, '😁'))
+
+    f.close() 
+
+if __name__ == '__main__' :
+    main()
